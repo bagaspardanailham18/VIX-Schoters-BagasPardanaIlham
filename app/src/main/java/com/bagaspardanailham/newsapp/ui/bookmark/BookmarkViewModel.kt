62911b9ba4b9ee4199fc226b4d8e2d9a25920e3e
bagaspardanailham18 <ibagaspardana@gmail.com>
2022-12-02 21:59:55 +0700

    first commit

---
+++ app/src/main/java/com/bagaspardanailham/newsapp/ui/bookmark/BookmarkViewModel.kt
@@ -0,0 +1,13 @@
+package com.bagaspardanailham.newsapp.ui.bookmark
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class BookmarkViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
